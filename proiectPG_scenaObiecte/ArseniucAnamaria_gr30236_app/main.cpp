ee189a7f366ee7129fdc99e003ea34b1d967d9c1
Anamaria Arseniuc <75796420+arseniucanamaria@users.noreply.github.com>
2021-03-29 20:13:09 +0300

    Add files via upload

---
+++ proiectPG_scenaObiecte/ArseniucAnamaria_gr30236_app/main.cpp
@@ -0,0 +1,426 @@
+﻿#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp> //core glm functionality
+#include <glm/gtc/matrix_transform.hpp> //glm extension for generating common transformation matrices
+#include <glm/gtc/matrix_inverse.hpp> //glm extension for computing inverse matrices
+#include <glm/gtc/type_ptr.hpp> //glm extension for accessing the internal data structure of glm types
+
+#include "Window.h"
+#include "Shader.hpp"
+#include "Camera.hpp"
+#include "Model3D.hpp"
+
+#include <iostream>
+
+// window
+gps::Window myWindow;
+int glWindowWidth=800;
+int glWindowHeight = 600;
+
+// matrices
+glm::mat4 model;
+glm::mat4 view;
+glm::mat4 projection;
+glm::mat3 normalMatrix;
+glm::mat4 modelDuck;
+
+// light parameters
+glm::vec3 lightDir;
+glm::vec3 lightColor;
+
+// shader uniform locations
+GLuint modelLoc;
+GLuint viewLoc;
+GLuint projectionLoc;
+GLuint normalMatrixLoc;
+GLuint lightDirLoc;
+GLuint lightColorLoc;
+
+// camera
+gps::Camera myCamera(
+    glm::vec3(0.0f, 0.0f, 3.0f),
+    glm::vec3(0.0f, 0.0f, -10.0f),
+    glm::vec3(0.0f, 1.0f, 0.0f));
+
+GLfloat cameraSpeed = 0.1f;
+
+GLboolean pressedKeys[1024];
+
+// models
+GLfloat angle;
+gps::Model3D scena;
+gps::Model3D duck;
+gps::Model3D drum;
+
+// shaders
+gps::Shader myBasicShader;
+
+//ADDED
+int retina_width, retina_height;
+GLFWwindow* glWindow = NULL;
+
+GLfloat X_axis = 36.6f;
+GLfloat Z_axis = 16.837f;
+
+//fog
+int activateTheFog = 0;
+
+
+
+
+GLenum glCheckError_(const char *file, int line)
+{
+	GLenum errorCode;
+	while ((errorCode = glGetError()) != GL_NO_ERROR) {
+		std::string error;
+		switch (errorCode) {
+            case GL_INVALID_ENUM:
+                error = "INVALID_ENUM";
+                break;
+            case GL_INVALID_VALUE:
+                error = "INVALID_VALUE";
+                break;
+            case GL_INVALID_OPERATION:
+                error = "INVALID_OPERATION";
+                break;
+            case GL_STACK_OVERFLOW:
+                error = "STACK_OVERFLOW";
+                break;
+            case GL_STACK_UNDERFLOW:
+                error = "STACK_UNDERFLOW";
+                break;
+            case GL_OUT_OF_MEMORY:
+                error = "OUT_OF_MEMORY";
+                break;
+            case GL_INVALID_FRAMEBUFFER_OPERATION:
+                error = "INVALID_FRAMEBUFFER_OPERATION";
+                break;
+        }
+		std::cout << error << " | " << file << " (" << line << ")" << std::endl;
+	}
+	return errorCode;
+}
+#define glCheckError() glCheckError_(__FILE__, __LINE__)
+
+void windowResizeCallback(GLFWwindow* window, int width, int height) {
+	fprintf(stdout, "Window resized! New width: %d , and height: %d\n", width, height);
+	//TODO
+    glfwGetFramebufferSize(window, &retina_width, &retina_height);
+    glViewport(0, 0, retina_width, retina_height);
+    projection = glm::perspective(glm::radians(45.0f), (float)retina_width / (float)retina_height, 0.1f, 1000.0f);
+    projectionLoc = glGetUniformLocation(myBasicShader.shaderProgram, "projection");
+    glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+}
+
+void keyboardCallback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+
+	if (key >= 0 && key < 1024) {
+        if (action == GLFW_PRESS) {
+            pressedKeys[key] = true;
+        } else if (action == GLFW_RELEASE) {
+            pressedKeys[key] = false;
+        }
+    }
+}
+
+
+GLfloat lastX = glWindowWidth / 2.0f;
+GLfloat lastY = glWindowHeight / 2.0f;
+GLfloat pitch = 0.0f;
+GLfloat yaw = 0.0f;
+GLboolean first_Mouse = true;
+GLfloat x_offset;
+GLfloat y_offset;
+
+void mouseCallback(GLFWwindow* window, double xpos, double ypos) {
+
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) != GLFW_PRESS) {
+        first_Mouse = true;
+        return;
+    }
+    if (first_Mouse)
+    {
+        first_Mouse = false;
+        lastX = xpos;
+        lastY = ypos;
+    }
+
+    float x_offset = xpos - lastX;
+    float y_offset = lastY - ypos;
+
+    lastX = xpos;
+    lastY = ypos;
+
+    x_offset *= cameraSpeed;
+    y_offset *= cameraSpeed;
+
+    myCamera.rotate(pitch + y_offset, yaw + x_offset);
+
+}
+
+void processMovement() {
+
+    if (pressedKeys[GLFW_KEY_W]) {
+        myCamera.move(gps::MOVE_FORWARD, cameraSpeed);
+        //update view matrix
+        view = myCamera.getViewMatrix();
+        myBasicShader.useShaderProgram();
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        // compute normal matrix for teapot
+        normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+    }
+
+    if (pressedKeys[GLFW_KEY_S]) {
+        myCamera.move(gps::MOVE_BACKWARD, cameraSpeed);
+        //update view matrix
+        view = myCamera.getViewMatrix();
+        myBasicShader.useShaderProgram();
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        // compute normal matrix for teapot
+        normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+    }
+
+    if (pressedKeys[GLFW_KEY_A]) {
+        myCamera.move(gps::MOVE_LEFT, cameraSpeed);
+        //update view matrix
+        view = myCamera.getViewMatrix();
+        myBasicShader.useShaderProgram();
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        // compute normal matrix for teapot
+        normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+    }
+
+    if (pressedKeys[GLFW_KEY_D]) {
+        myCamera.move(gps::MOVE_RIGHT, cameraSpeed);
+        //update view matrix
+        view = myCamera.getViewMatrix();
+        myBasicShader.useShaderProgram();
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        // compute normal matrix for teapot
+        normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+    }
+
+    //invartire scena la stanga
+    if (pressedKeys[GLFW_KEY_Q]) {
+        angle -= 1.0f;
+        // update model matrix for teapot
+        model = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(0, 1, 0));
+        // update normal matrix for teapot
+        normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+    }
+
+    //invartire scena la dreapta
+    if (pressedKeys[GLFW_KEY_E]) {
+        angle += 1.0f;
+        // update model matrix for teapot
+        model = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(0, 1, 0));
+        // update normal matrix for teapot
+        normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+    }
+
+    //pentru ceata
+        //ACTIVARE ceata
+    if (pressedKeys[GLFW_KEY_C]) {
+        activateTheFog = 1;
+        myBasicShader.useShaderProgram();
+        glUniform1i(glGetUniformLocation(myBasicShader.shaderProgram, "activateTheFog"), activateTheFog);
+    }
+
+        //DEZACTIVARE ceata
+    if (pressedKeys[GLFW_KEY_X]) {
+        activateTheFog = 0;
+        myBasicShader.useShaderProgram();
+        glUniform1i(glGetUniformLocation(myBasicShader.shaderProgram, "activateTheFog"), activateTheFog);
+    }
+
+    //SOLID, WIREFRAME, POLIGONAL
+    if (pressedKeys[GLFW_KEY_N]) {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    }
+  
+    if (pressedKeys[GLFW_KEY_M]) {
+           glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
+    }
+   
+    if (pressedKeys[GLFW_KEY_B]) {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    }
+    
+
+    //for the duck (ANIMATIE)
+
+    if (pressedKeys[GLFW_KEY_6]) {
+        X_axis += 0.01f;
+    }
+    if (pressedKeys[GLFW_KEY_7]) {
+        X_axis -= 0.01f;
+    }
+
+    if (pressedKeys[GLFW_KEY_8]) {
+        Z_axis += 0.01f;
+    }
+    if (pressedKeys[GLFW_KEY_9]) {
+        Z_axis -= 0.01f;
+    }
+
+}
+
+void initOpenGLWindow() {
+    myWindow.Create(1024, 768, "OpenGL Project Core");
+}
+
+void setWindowCallbacks() {
+	glfwSetWindowSizeCallback(myWindow.getWindow(), windowResizeCallback);
+    glfwSetKeyCallback(myWindow.getWindow(), keyboardCallback);
+    glfwSetCursorPosCallback(myWindow.getWindow(), mouseCallback);
+}
+
+void initOpenGLState() {
+	glClearColor(0.7f, 0.7f, 0.7f, 1.0f);
+	glViewport(0, 0, myWindow.getWindowDimensions().width, myWindow.getWindowDimensions().height);
+    glEnable(GL_FRAMEBUFFER_SRGB);
+	glEnable(GL_DEPTH_TEST); // enable depth-testing
+	glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
+	glEnable(GL_CULL_FACE); // cull face
+	glCullFace(GL_BACK); // cull back face
+	glFrontFace(GL_CCW); // GL_CCW for counter clock-wise
+}
+
+void initModels() {
+    scena.LoadModel("models/objects/scena4.obj");
+    duck.LoadModel("models/objects/duck1.obj");
+    drum.LoadModel("models/objects/drum.obj");
+}
+
+void initShaders() {
+	myBasicShader.loadShader(
+        "shaders/basic.vert",
+        "shaders/basic.frag");
+}
+
+void initUniforms() {
+	myBasicShader.useShaderProgram();
+
+    // create model matrix
+    model = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(0.0f, 1.0f, 0.0f));
+	modelLoc = glGetUniformLocation(myBasicShader.shaderProgram, "model");
+
+	// get view matrix for current camera
+	view = myCamera.getViewMatrix();
+	viewLoc = glGetUniformLocation(myBasicShader.shaderProgram, "view");
+	// send view matrix to shader
+    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+    // compute normal matrix 
+    normalMatrix = glm::mat3(glm::inverseTranspose(view*model));
+	normalMatrixLoc = glGetUniformLocation(myBasicShader.shaderProgram, "normalMatrix");
+
+	// create projection matrix
+	projection = glm::perspective(glm::radians(45.0f),
+                               (float)myWindow.getWindowDimensions().width / (float)myWindow.getWindowDimensions().height,
+                               0.1f, 20.0f);
+	projectionLoc = glGetUniformLocation(myBasicShader.shaderProgram, "projection");
+	// send projection matrix to shader
+	glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));	
+
+	//set the light direction (direction towards the light)
+	lightDir = glm::vec3(0.0f, 1.0f, 1.0f);
+	lightDirLoc = glGetUniformLocation(myBasicShader.shaderProgram, "lightDir");
+	// send light dir to shader
+	glUniform3fv(lightDirLoc, 1, glm::value_ptr(lightDir));
+
+	//set light color
+	lightColor = glm::vec3(1.0f, 1.0f, 1.0f); //white light
+	lightColorLoc = glGetUniformLocation(myBasicShader.shaderProgram, "lightColor");
+	// send light color to shader
+	glUniform3fv(lightColorLoc, 1, glm::value_ptr(lightColor));
+}
+
+void renderScena(gps::Shader shader) {
+    // select active shader program
+    shader.useShaderProgram();
+
+    //send 'scena' model matrix data to shader
+    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+    //send 'scena' normal matrix data to shader
+    glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, glm::value_ptr(normalMatrix));
+
+    // draw 'scena'
+    scena.Draw(shader);
+}
+
+void renderDuck(gps::Shader shader) {
+    shader.useShaderProgram();
+    modelDuck = glm::mat4(1.0f);
+    modelDuck = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(0.0f, 1.0f, 0.0f));
+    modelDuck = glm::translate(modelDuck, glm::vec3(X_axis, 1.001, Z_axis)); //y-constant
+    modelDuck = glm::scale(modelDuck, glm::vec3(0.5f));
+    glUniformMatrix4fv(glGetUniformLocation(shader.shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(modelDuck));
+    duck.Draw(shader);
+}
+
+void renderDrum(gps::Shader shader) {
+    // select active shader program
+    shader.useShaderProgram();
+
+    //send 'drum' model matrix data to shader
+    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+    //send 'drum' normal matrix data to shader
+    glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, glm::value_ptr(normalMatrix));
+
+    // draw 'drum'
+    drum.Draw(shader);
+}
+
+void renderScene() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//render the scene, duck and road
+	renderScena(myBasicShader);
+    renderDuck(myBasicShader);
+    renderDrum(myBasicShader);
+}
+
+void cleanup() {
+    myWindow.Delete();
+    //cleanup code for your own data
+}
+
+int main(int argc, const char * argv[]) {
+
+    try {
+        initOpenGLWindow();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    initOpenGLState();
+	initModels();
+	initShaders();
+	initUniforms();
+    setWindowCallbacks();
+
+	glCheckError();
+	// application loop
+	while (!glfwWindowShouldClose(myWindow.getWindow())) {
+        processMovement();
+	    renderScene();
+
+		glfwPollEvents();
+		glfwSwapBuffers(myWindow.getWindow());
+
+		glCheckError();
+	}
+
+	cleanup();
+
+    return EXIT_SUCCESS;
+}
